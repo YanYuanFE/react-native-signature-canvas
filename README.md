e1e1b5d2d3fda44792b72b1cc02e89908ebf91e2
YanYuan <1025138284@qq.com>
2018-12-02 22:50:45 +0800

    update1.1,delete react-navigation

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # react-native-signature-canvas
+
 React Native Signature Component based Canvas for Android &amp;&amp; IOS &amp;&amp; expo
 
 - Supports Android and iOS and Expo
 - Pure JavaScript implementation with no native dependencies
 - Tested with RN 0.50
 - Core use [signature_pad.js](https://github.com/szimek/signature_pad)
-- Only depend on react and react native and react-navigation
+- Only depend on react and react native
 - Generates a base64 encoded png image of the signature
 
 ## Installation
@@ -16,87 +17,61 @@ npm install --save react-native-signature-canvas
 
 ## Example
 
+![Signature](http://img.yanyuanfe.cn/reactnativesinature.jpeg)
+
 ```js
-import SignatureScreen from 'react-native-signature-canvas';
-import DemoScreen from './Demo';
-const ROUTES = {
-    // other screen
-    SignatureScreen,
-    DemoScreen
-}
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import SignatureScreen from "react-native-signature-canvas";
 
-const AppNavigator = StackNavigator(
-    {
-        ...ROUTES
-        // ,
-        // Index: {
-        //     screen: Index,
-        // },
-    },
-    {
-        initialRouteName: 'DemoScreen',
-        mode: Platform.OS === 'ios' ? 'modal' : 'card',
-        cardStyle: {
-            backgroundColor: '#fff'
-        }
-    }
-);
-
-import React from 'react';
-import {StyleSheet} from 'react-native';
-var {
-  View,
-  Component,
-} = React;
-
-export default class Demo extends Component {
-  startSignature = () => {
-    this.props.navigation.navigate("SignatureScreen", {
-        title: "签字",
-        callback: (data) => {
-          this.setState({
-              signature: data
-          });
-        }
-    });
+export default class Signature extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { signature: null };
   }
 
-  render = () => {
-    return (
-      <View style={{flex: 1}}>
-        <View style={styles.preview}>
-            {this.state.signature ? <Image
-                resizeMode={'contain'}
-                style={{ width: 335, height: 114 }}
-                source={{ uri: this.state.signature }}
-            /> : null}
-        </View>
-        <Text style={styles.previewText} onPress={this.startSignature}>开始签字</Text>
-      </View>
-    )
+  handleSignature = signature => {
+    this.setState({ signature });
   };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.preview}>
+          {this.state.signature ? (
+            <Image
+              resizeMode={"contain"}
+              style={{ width: 335, height: 114 }}
+              source={{ uri: this.state.signature }}
+            />
+          ) : null}
+        </View>
+        <SignatureScreen onOK={this.handleSignature} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   preview: {
     width: 335,
     height: 114,
-    backgroundColor: '#F8F8F8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 15,
+    backgroundColor: "#F8F8F8",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15
   },
   previewText: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 14,
     height: 40,
     lineHeight: 40,
     paddingLeft: 10,
     paddingRight: 10,
-    backgroundColor: '#69B2FF',
+    backgroundColor: "#69B2FF",
     width: 120,
-    textAlign: 'center',
-    marginTop: 10,
-  },
-})
+    textAlign: "center",
+    marginTop: 10
+  }
+});
 ```
--- index.js
+++ index.js
@@ -1,59 +1,48 @@
-import React, {Component} from 'react';
-import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-    Button,
-    WebView,
-    Platform
-} from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, WebView } from "react-native";
 
-import { StackNavigator, NavigationActions } from 'react-navigation';
-
-import htmlContent from './h5/html';
-import injectedSignaturePad from './h5/js/signature_pad';
-import injectedApplication from './h5/js/app';
+import htmlContent from "./h5/html";
+import injectedSignaturePad from "./h5/js/signature_pad";
+import injectedApplication from "./h5/js/app";
 
 const styles = StyleSheet.create({
-    signature:{
-        width:200,
-        height:110,
-        borderWidth:2,
-        borderColor:'grey'
-    },
-    signaturBg: {
-        alignItems:'center',
-        marginTop:20
-    },
-    webView :{
-    },
-    webBg: {
-        backgroundColor:'red',
-        flex:1
-    }
+  signature: {
+    width: 200,
+    height: 110,
+    borderWidth: 2,
+    borderColor: "grey"
+  },
+  signaturBg: {
+    alignItems: "center",
+    marginTop: 20
+  },
+  webView: {},
+  webBg: {
+    width: "100%",
+    paddingTop: 20,
+    backgroundColor: "#FFF",
+    flex: 1
+  }
 });
 
 class SignatureView extends Component {
+  static defaultProps = {
+    style: styles.webView,
+    webBgStyle: styles.webBg,
+    activeOpacity: 0.8
+  };
 
-    static defaultProps = {
-        style: styles.webView,
-        webBgStyle: styles.webBg,
-        activeOpacity: 0.8
-    }
+  constructor(props) {
+    super(props);
+    this.state = { base64DataUrl: props.dataURL || null };
+    const { backgroundColor } = StyleSheet.flatten(props.style);
+    const injectedJavaScript = injectedSignaturePad + injectedApplication;
+    const html = htmlContent(injectedJavaScript);
+    this.source = { html };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {base64DataUrl: props.dataURL || null};
-        const { backgroundColor } = StyleSheet.flatten(props.style);
-        const injectedJavaScript = injectedSignaturePad + injectedApplication;
-        const html = htmlContent(injectedJavaScript);
-        this.source = {html};
-    }
-
-    state = {
-        bridgeJs: `
+  state = {
+    bridgeJs: `
         (function ready() {
           saveButton.addEventListener("click", function (event) {
             if (signaturePad.isEmpty()) {
@@ -63,42 +52,30 @@ class SignatureView extends Component {
             }
           });
         })();`
-    }
+  };
 
-    static  navigationOptions = ({navigation}) => ({
-        title:navigation.state.params.title,
-        mode:'modal',
-        headerTitleStyle: {marginLeft:(Platform.OS === 'ios')?0:106},
-    })
+  getSignature = e => {
+    const { onOK } = this.props;
+    onOK(e.nativeEvent.data);
+  };
 
-    getSignature = (e) => {
-        const {navigate,goBack,state} = this.props.navigation;
-        state.params.callback(e.nativeEvent.data);
-        goBack();
-        // callback(e.nativeEvent.data);
-    }
+  _renderError = args => {
+    console.log("error", args);
+  };
 
-    _renderError = (args) => {
-        console.log('error', args)
-        alert(JSON.stringify(args));
-    };
-
-    render(){
-        let { callback, webBgStyle, style, activeOpacity } = this.props;
-        const source = (Platform.OS == 'ios') ? require('./h5/index.html') : { uri: 'file:///android_asset/h5/index.html' }
-        return(
-            <View style={styles.webBg}>
-                <WebView
-                    style={styles.webView}
-                    source={this.source}
-                    onMessage={this.getSignature}
-                    javaScriptEnabled={true}
-                    onError={this._renderError}
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.webBg}>
+        <WebView
+          style={styles.webView}
+          source={this.source}
+          onMessage={this.getSignature}
+          javaScriptEnabled={true}
+          onError={this._renderError}
+        />
+      </View>
+    );
+  }
 }
-
 
 export default SignatureView;
