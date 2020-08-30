2777222e90cf2442b57ac1cd74a59e44038319f5
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-08-30 14:38:58 -0700

    Update signapp example react-native to 0.63.2

--- example/signapp/App.js
+++ example/signapp/App.js
@@ -1,65 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-// import SignatureScreen from 'react-native-signature-canvas';
-import { WebView } from 'react-native-webview';
+import React, {useState} from 'react';
+import {Image, StyleSheet, View} from 'react-native';
 
+import SignatureScreen from 'react-native-signature-canvas';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { signature: null };
-  }
-
-  handleSignature = signature => {
-    console.log(signature);
-    this.setState({ signature });
-  };
-
-  handleEmpty = () => {
-    console.log('Empty');
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={styles.preview}>
-          {this.state.signature ? (
-            <Image
-              resizeMode={"contain"}
-              style={{ width: 335, height: 114 }}
-              source={{ uri: this.state.signature }}
-            />
-          ) : null}
-        </View>
-        <WebView
-        source={{ uri: 'https://infinite.red' }}
-        style={{ marginTop: 20 }}
-      />
-        {/* <SignatureScreen onOK={this.handleSignature} onEmpty={this.handleEmpty} autoClear={true} imageType={"image/svg+xml"} /> */}
+const App: () => React$Node = () => {
+  const [signature, setSignature] = useState(null);
+  return (
+    <>
+      <View style={styles.preview}>
+        {signature && (
+          <Image style={styles.previewImage} source={{uri: signature}} />
+        )}
       </View>
-    );
-  }
-}
+      <SignatureScreen
+        onOK={setSignature}
+        onEmpty={() => console.log('onEmpty')}
+        onClear={() => setSignature(null)}
+        autoClear={true}
+        imageType={'image/png+xml'}
+      />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   preview: {
     height: 114,
-    backgroundColor: "#F8F8F8",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 15,
     flex: 1,
   },
-  previewText: {
-    color: "#FFF",
-    fontSize: 14,
-    height: 40,
-    lineHeight: 40,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: "#69B2FF",
-    width: 120,
-    textAlign: "center",
-    marginTop: 10
-  }
+  previewImage: {
+    width: 335,
+    height: 114,
+    resizeMode: 'contain',
+  },
 });
+
+export default App;
