0d36f571461501fcfd392d4449e8a786314a576f
yanyuan <yan950414@gmail.com>
2020-10-20 22:13:41 +0800

    fix: example

--- example/expoApp/App.js
+++ example/expoApp/App.js
@@ -1,59 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import SignatureScreen from 'react-native-signature-canvas';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignScreen from "./SignScreen";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { signature: null };
-  }
-
-  handleSignature = signature => {
-    console.log(signature);
-    this.setState({ signature });
-  };
-
-  handleEmpty = () => {
-    console.log('Empty');
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={styles.preview}>
-          {this.state.signature ? (
-            <Image
-              resizeMode={"contain"}
-              style={{ width: 335, height: 114 }}
-              source={{ uri: this.state.signature }}
-            />
-          ) : null}
-        </View>
-        <SignatureScreen onOK={this.handleSignature} onEmpty={this.handleEmpty} />
-      </View>
-    );
-  }
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Sign"
+        onPress={() => navigation.navigate('Sign')}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  preview: {
-    height: 114,
-    backgroundColor: "#F8F8F8",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 15,
-    flex: 1,
-  },
-  previewText: {
-    color: "#FFF",
-    fontSize: 14,
-    height: 40,
-    lineHeight: 40,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: "#69B2FF",
-    width: 120,
-    textAlign: "center",
-    marginTop: 10
-  }
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sign" component={SignScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
