1d18ec7db7dccbc12b529fea42f0c33687b7fbd1
leeilhyung <leeih83@gmail.com>
2021-06-10 15:08:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ const SignatureView = forwardRef(({
   minWidth = 0.5,
   androidHardwareAccelerationDisabled = false,
   style = null,
+  webviewContainerStyle = null,
+  scrollable = false,
 }, ref) => {
   const [loading, setLoading] = useState(true);
   const webViewRef = useRef();
@@ -104,6 +106,8 @@ const SignatureView = forwardRef(({
     <View style={[styles.webBg, style]}>
       <WebView
         bounces={false}
+        style={[webviewContainerStyle]}
+        scrollEnabled={scrollable}
         androidHardwareAccelerationDisabled={androidHardwareAccelerationDisabled}
         ref={webViewRef}
         useWebKit={true}
