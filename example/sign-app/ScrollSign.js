49f9b21df188125781208a558ee1db9398e3a357
YanYuan <yan950414@gmail.com>
2023-08-24 21:32:24 +0800

    chore: update example for scroll

---
+++ example/sign-app/ScrollSign.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import SignatureCanvas, { SignatureViewRef } from 'react-native-signature-canvas';
+
+export const MySignatureCanvas = ({setScroll}) => {
+    const signatureRef = useRef(null); // Create a ref for the signature canvas
+
+    const handleClearSignature = () => {
+        signatureRef.current?.clearSignature();
+    };
+
+    const handleGetSignature = () => {
+        const signatureData = signatureRef.current?.getData();
+        console.log(signatureData);
+    };
+
+    return (
+        <SignatureCanvas
+            androidHardwareAccelerationDisabled={true}
+            ref={signatureRef}
+            scrollable={false} // Disable scrolling
+            // backgroundColor="#000"
+            maxWidth={3}
+            style={styles.canvas}
+            onBegin={() => setScroll(false)}
+            onEnd={() => setScroll(true)}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    canvas: {
+        borderWidth: 1,
+        borderColor: 'red',
+        width: '90%',
+        height: 300, // Increase the height for more drawing space
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+    },
+});
